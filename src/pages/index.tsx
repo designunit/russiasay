32bb487570e106aee0f73c36726573037f51fb3f
manhir <53478716+manhir@users.noreply.github.com>
2021-04-28 13:30:17 +0300

    requested by artour yurich

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,28 +30,6 @@ const Index: NextPage = () => {
             text: 'контакты',
             content: <Contacts />,
         },
-        {
-            text: (
-                <Link
-                    href='https://youtu.be/mOnchN3hQSk'
-                >
-                    <a style={{
-                        display: 'flex',
-                        alignItems: 'center'
-                    }}>
-                        Видео
-                        <svg xmlns="http://www.w3.org/2000/svg" className="icon" viewBox="0 0 1024 1024" style={{
-                            height: '1.5rem',
-                            marginLeft: '.5rem',
-                            fill: isMobile ? 'white' : 'black'
-                        }}>
-                            <path d="M941.3 296.1a112.3 112.3 0 0 0-79.2-79.3C792.2 198 512 198 512 198s-280.2 0-350.1 18.7A112.12 112.12 0 0 0 82.7 296C64 366 64 512 64 512s0 146 18.7 215.9c10.3 38.6 40.7 69 79.2 79.3C231.8 826 512 826 512 826s280.2 0 350.1-18.8c38.6-10.3 68.9-40.7 79.2-79.3C960 658 960 512 960 512s0-146-18.7-215.9zM423 646V378l232 133-232 135z" />
-                        </svg>
-                    </a>
-                </Link>
-            ),
-            content: null
-        },
     ]
 
     return (
